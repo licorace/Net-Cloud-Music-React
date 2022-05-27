67f98f7ce931719076782e20adf5b748e67904e5
licorace <13918539500@163.com>
2022-05-27 17:27:21 +0800

    style(add config): add basic config before coding

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {}
+}
